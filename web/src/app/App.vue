a241a598109e9d41b878d44e1a854dfc6dc748e4
dong <1438318243@qq.com>
2026-08-01 17:17:00 +0800

    fix(web): harden errors and sensitive data handling

--- web/src/app/App.vue
+++ web/src/app/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import GlobalNotificationHost from '@/components/shared/GlobalNotificationHost.vue'
 </script>
 
 <template>
   <RouterView />
+  <GlobalNotificationHost />
 </template>
